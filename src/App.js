d8375fa0ab4f9c59e3b83cd3eaeb4f6c2a86b323
Aziz Moiz <aziz@bongolive.co.tz>
2021-09-26 23:54:58 +0300

    added nasa apod api

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Resume from './components/Resume';
 import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Nasa from './components/Nasa';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/resume" component={Resume} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
+        <Route exact path="/nasa" component={Nasa} />
       </Switch>
     </React.Fragment>
   );
---
+++ src/components/Nasa.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { NASA_API_KEY, NASA_APOD_URL } from '../config';
+import '../styles/Nasa.css';
+
+const url = NASA_APOD_URL;
+const api_key = NASA_API_KEY;
+
+const Nasa = () => {
+  const [photoData, setPhotoData] = useState(null);
+
+  useEffect(() => {
+    fetchNASAData();
+    async function fetchNASAData() {
+      try {
+        const response = await fetch(`${url}${api_key}`);
+        const data = await response.json();
+        console.log('NASA APOD data', data);
+        setPhotoData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, []);
+
+  if (!photoData) return <div />;
+
+  return (
+    <>
+      <div className="nasa-photo">
+        {photoData.media_type === 'image' ? (
+          <img src={photoData.url} alt={photoData.title} className="photo" />
+        ) : (
+          <iframe
+            title="space-video"
+            src={photoData.url}
+            frameBorder="0"
+            gesture="media"
+            allow="encrypted-media"
+            allowFullScreen
+            className="photo"
+          />
+        )}
+        <div>
+          <h1>{photoData.title}</h1>
+          <p className="date">{String(new Date(photoData.date))}</p>
+          <p className="explanation">{photoData.explanation}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Nasa;
